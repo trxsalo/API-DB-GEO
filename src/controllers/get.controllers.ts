a62c0067a387a7990e433f3e96754d072533356d
trxsalome <marcosarauz46@gmail.com>
2022-11-14 15:05:21 -0500

    el readme y consulta

--- src/controllers/get.controllers.ts
+++ src/controllers/get.controllers.ts
@@ -8,13 +8,12 @@ import { QueryResult } from "pg";
  * Aqui se consulta a las base de datos para regresar la  informacion en un formato json
  *  
  */
-
 export const getConsulta= async (req:Request, res:Response):Promise<Response>=>{
 
     try{ //Con el try realizamos que si alla un error devuelve una respuesta para que no se quede esperado una respuesta 
-        
-        
-        return res.status(200);
+        const con = await connet(); 
+        const lote:QueryResult = await con.query('SELECT row_to_json(fc) FROM ( SELECT array_to_json(array_agg(f)) As Datos FROM (SELECT ST_AsGeoJSON(lg.area)::json As geometry, row_to_json((loteid, name_code)) As properties FROM lote As lg) As f) As fc');
+        return res.status(200).json(lote.rows)
     }
 
     catch(e){
