0661f9c5611c35d190bbac7dbfcfebf01f0fb722
trxsalo <marcosarauz46@gmail.com>
2022-11-28 09:05:18 -0400

    correccion de consulta en los constrollers

--- src/controllers/get.controllers.ts
+++ src/controllers/get.controllers.ts
@@ -13,7 +13,7 @@ export const getLote = async (req:Request, res:Response):Promise<Response>=>{
     try{ //Con el try realizamos que si alla un error devuelve una respuesta para que no se quede esperado una respuesta 
 
         const con = await connet();
-        const lote:QueryResult = await con.query('SELECT row_to_json(fc) FROM ( SELECT array_to_json(array_agg(f)) As Datos FROM (SELECT ST_AsGeoJSON(lg.area)::json As geometry, row_to_json((loteid,estado,calleid)) As properties FROM lote As lg) As f) As fc');
+        const lote:QueryResult = await con.query('SELECT row_to_json(fc) FROM ( SELECT array_to_json(array_agg(f)) As Datos FROM (SELECT ST_AsGeoJSON(ST_Transform(lg.area,4326))::json As geometry, row_to_json((loteid,estado,calleid)) As properties FROM lote As lg) As f) As fc');
         //const lote:QueryResult = await con.query('SELECT * FROM public.lote ORDER BY loteid ASC');
         //const prueba:QueryResult= await con.query('SELECT * FROM prueba');
         return res.status(200).json(lote.rows);
@@ -49,7 +49,28 @@ export const getManzano = async (req:Request, res:Response):Promise<Response>=>{
     try{ //Con el try realizamos que si alla un error devuelve una respuesta para que no se quede esperado una respuesta
 
         const con = await connet();
-        const lote:QueryResult = await con.query('SELECT * FROM public.manzano ORDER BY manzanoid ASC ');
+        const manzano:QueryResult = await con.query('SELECT row_to_json(fc) FROM ( SELECT array_to_json(array_agg(f)) As Datos FROM (SELECT ST_AsGeoJSON(ST_Transform(lg.area,4326))::json As geometry, row_to_json((manzanoid,name_code)) As properties FROM manzano As lg) As f) As fc');
+        //const lote:QueryResult = await con.query('SELECT * FROM public.lote ORDER BY loteid ASC');
+        //const prueba:QueryResult= await con.query('SELECT * FROM prueba');
+        return res.status(200).json(manzano.rows);
+    }
+
+    catch(e){
+        return res.status(500).json({
+            message:'Error interno'
+        })
+    }
+
+}
+
+export const getCalle = async (req:Request, res:Response):Promise<Response>=>{
+
+    try{ //Con el try realizamos que si alla un error devuelve una respuesta para que no se quede esperado una respuesta
+
+        const con = await connet();
+        const lote:QueryResult = await con.query('SELECT row_to_json(fc) FROM ( SELECT array_to_json(array_agg(f)) As Datos FROM (SELECT ST_AsGeoJSON(ST_Transform(lg.longitud,4326))::json As geometry, row_to_json((calleid,name)) As properties FROM calle As lg) As f) As fc');
+        //const lote:QueryResult = await con.query('SELECT * FROM public.lote ORDER BY loteid ASC');
+        //const prueba:QueryResult= await con.query('SELECT * FROM prueba');
         return res.status(200).json(lote.rows);
     }
 
@@ -61,3 +82,21 @@ export const getManzano = async (req:Request, res:Response):Promise<Response>=>{
 
 }
 
+export const getLuces = async (req:Request, res:Response):Promise<Response>=>{
+
+    try{ //Con el try realizamos que si alla un error devuelve una respuesta para que no se quede esperado una respuesta
+
+        const con = await connet();
+        const lote:QueryResult = await con.query('SELECT row_to_json(fc) FROM ( SELECT array_to_json(array_agg(f)) As Datos FROM (SELECT ST_AsGeoJSON(ST_Transform(lg.punto,4326))::json As geometry, row_to_json((lucesid,estado,tipo)) As properties FROM luces As lg) As f) As fc');
+        //const lote:QueryResult = await con.query('SELECT * FROM public.lote ORDER BY loteid ASC');
+        //const prueba:QueryResult= await con.query('SELECT * FROM prueba');
+        return res.status(200).json(lote.rows);
+    }
+
+    catch(e){
+        return res.status(500).json({
+            message:'Error interno'
+        })
+    }
+
+}
